0f46e0053415a406d579675c7ea4dd02305f1efa
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-08-10 16:44:39 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,8 +7,9 @@ def key_for_min_value(name_hash)
   smallest_max = 1000000
 
   name_hash.each do |key, value|
-    if value <= smallest_max
       smallest_max = value
+
+    if value <= smallest_max
       smallest_min = key
     end
   end
